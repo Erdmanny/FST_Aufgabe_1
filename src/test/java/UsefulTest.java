0b1a0265a825226acc7e6c55d6cf8836b858073d
Erdmanny <Michael.becker99@outlook.de>
2022-11-04 21:49:39 +0100

    Exercise 1

---
+++ src/test/java/UsefulTest.java
@@ -0,0 +1,24 @@
+import de.unitrier.st.core.CountryDataPoint;
+import de.unitrier.st.core.DataLoader;
+import de.unitrier.st.core.DataParser;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+public class UsefulTest {
+
+    @Test
+    public void uTest() {
+        DataLoader dataLoader = new DataLoader();
+        DataParser dataParser = new DataParser();
+        String json = dataLoader.loadJsonFile("countries-aggregated_json.json");
+        Map<String, List<CountryDataPoint>> countries = dataParser.parseJsonData(json);
+
+        Assert.assertNotSame(countries, Collections.emptyMap());
+    }
+
+}
